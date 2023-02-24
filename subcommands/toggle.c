eacde3d36d10a1f56d4627a14e2dd5b11161bf98
AndreasHGK <andreaselbergs@outlook.com>
2023-02-24 10:22:33 +0100

    subcommand/toggle.c: Print newline at the end of a msg

--- subcommands/toggle.c
+++ subcommands/toggle.c
@@ -10,7 +10,7 @@
 int subcommand_toggle(const struct CommandOpts *opts) {
     struct TodoList list = {};
     if (!read_list(&list, opts->filepath)) {
-        fprintf(stderr, "Could not read file `%s` or it does not exist.", opts->filepath);
+        fprintf(stderr, "Could not read file `%s` or it does not exist.\n", opts->filepath);
         return 1;
     }
 
@@ -21,17 +21,17 @@ int subcommand_toggle(const struct CommandOpts *opts) {
     errno = 0;
     long entry_num = strtol(buf, NULL, 10);
     if (errno != 0) {
-        fprintf(stderr, "Could not parse `%s` as an integer.", buf);
+        fprintf(stderr, "Could not parse `%s` as an integer.\n", buf);
         return 1;
     }
 
     if (entry_num >= list.count || entry_num < 0) {
-        fprintf(stderr, "That entry does not exist!");
+        fprintf(stderr, "That entry does not exist!\n");
         return 1;
     }
 
     list.entries[entry_num].completed = !list.entries[entry_num].completed;
-    printf("Success! The following entry has been marked as %s:\n`%s`",
+    printf("Success! The following entry has been marked as %s:\n`%s`\n",
            list.entries[entry_num].completed ? "completed" : "incomplete", list.entries[entry_num].desc);
 
     write_list(&list, opts->filepath);
